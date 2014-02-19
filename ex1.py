b999957ac9f214ee3ec3bdc092970f6aa6929d25
MariaBT <Mariabthomassen@gmail.com>
2014-02-19 21:44:35 +0100

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello world!"
+print "Hello again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "and we have another line"
+# Type "#" in the beginning of any lines above, and they won't get printed
