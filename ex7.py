33fc95ccc45dd3066f959aab264cd4e4a112ebc3
MariaBT <Mariabthomassen@gmail.com>
2014-02-19 21:57:17 +0100

    ex7.py

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # multiplies, so that the "." gets repeated 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# When we want to write a word with two parts on the same line, we use comma at the end of the first print
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# Without the comma the word would've been parted in two lines
