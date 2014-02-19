daceb6158f286c18330eb0598d8282f2479ea602
MariaBT <Mariabthomassen@gmail.com>
2014-02-19 21:51:05 +0100

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# Only text describing what will be done 
+print "I will count my chickens:"
+
+# Print the result of the number of hens
+print "Hens", 25 + 30 / 6
+
+# Print the result of the number of roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+# Plain text explaining  what will be done next
+print "Now I will count the eggs:"
+
+# The result of the action above 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print a question of plain text
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Print the answer: true or false
+print 3 + 2 < 5 - 7
+
+# Print text, followed by an answer
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 - 7?", 5 - 7
+
+# Print text 
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+# Print text in form of questions, followed by an answer: true og false
+print "Is it greater?", 5> -2
+print "Is it greater og equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Should add ".0" to every number to get more accurate results (floating point numbers)
